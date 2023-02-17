019ed7b9a902d259a5297f8933195d6e343bb7e0
dabit3 <dabit3@gmail.com>
2023-02-17 09:32:04 -0600

    testing func size

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Exm } from '@execution-machine/sdk'
+// import { Exm } from '@execution-machine/sdk'
 import { verifyMessage } from 'ethers'
 import { functionId } from '../../exm/functionId.js'
 
 const EXM_API_KEY = process.env.EXM_API_KEY 
 const API_KEY = process.env.NEXT_PUBLIC_GC_API_KEY
 const COMMUNITY_ID = process.env.NEXT_PUBLIC_GC_COMMUNITY_ID
-const exmInstance = EXM_API_KEY ? new Exm({ token: EXM_API_KEY }) : undefined
+// const exmInstance = EXM_API_KEY ? new Exm({ token: EXM_API_KEY }) : undefined
 
 const headers = API_KEY ? ({
   'Content-Type': 'application/json',
@@ -22,7 +22,7 @@ export default async function handler(
   req: any,
   res: any
 ) {
-  if (exmInstance) {
+  if (true) {
     try {
       let address, signature, formData
       if (req.body) {
@@ -32,25 +32,25 @@ export default async function handler(
         formData = body.formData
       }
       const GET_PASSPORT_SCORE_URI = `https://api.scorer.gitcoin.co/registry/score/${COMMUNITY_ID}/${address}`
-      const exmdata = await exmInstance.functions.read(functionId)
-      const nonce = exmdata['users'][address]['nonce']
-      const decodedAddress = verifyMessage(nonce, signature)
-      if(address.toLowerCase() === decodedAddress.toLowerCase()) {
-        const response = await fetch(GET_PASSPORT_SCORE_URI, {
-          headers
-        })
-        const passportData = await response.json()
-        if (parseInt(passportData.score) >= 32) {
-          const inputs = [{
-            type: 'setFormData',
-            address,
-            formData
-          }]
-          await exmInstance.functions.write(functionId, inputs)
-          res.status(200).json({ status: 'success' })
-        } else {
-          res.status(200).json({ status: 'failure', error: 'score did not meet threshold' })
-        }
+      // const exmdata = await exmInstance.functions.read(functionId)
+      // const nonce = exmdata['users'][address]['nonce']
+      // const decodedAddress = verifyMessage(nonce, signature)
+      // if(address.toLowerCase() === decodedAddress.toLowerCase()) {
+      //   const response = await fetch(GET_PASSPORT_SCORE_URI, {
+      //     headers
+      //   })
+      //   const passportData = await response.json()
+      //   if (parseInt(passportData.score) >= 32) {
+      //     const inputs = [{
+      //       type: 'setFormData',
+      //       address,
+      //       formData
+      //     }]
+      //     await exmInstance.functions.write(functionId, inputs)
+      //     res.status(200).json({ status: 'success' })
+      //   } else {
+      //     res.status(200).json({ status: 'failure', error: 'score did not meet threshold' })
+      //   }
       } else {
         res.status(200).json({
           status: 'error',
